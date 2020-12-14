f607aa51774412c326c8d8e55f32b7fe9a44b3a5
Stefan Eng <eng_steff@outlook.com>
2020-12-14 23:51:49 +0100

    Detach the qutebrowser process.

--- site.sh
+++ site.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #qutebrowser https://craftinginterpreters.com/scanning.html&
 URL=https://craftinginterpreters.com/representing-code.html
-qutebrowser --target window $URL
+qutebrowser --target window $URL&
